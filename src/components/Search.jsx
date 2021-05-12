10246520d81594f08a1067ddfb76cd0090f776d5
manavarrp <manavarrp@gmail.com>
2021-05-12 13:23:15 -0500

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,43 @@
+
+import styles from "./Search.module.css"
+import { FaSearchengin } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { useQuery } from "../hooks/useQuery";
+
+
+
+
+
+export function Search() {
+  
+    const query = useQuery();
+    const search = query.get("search");
+    const [searchText, setSearchText] = useState("");
+    const history = useHistory();
+
+    useEffect(() => {
+        setSearchText(search || "");
+     }, [search]);
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+    history.push("/?search=" + searchText);
+    };
+
+
+
+    return (
+        <form className={styles.searchContainer} onSubmit={handleSubmit} >
+            <div className={styles.searchBox}>
+              <input 
+                className={styles.searchInput} 
+                type="text" value={searchText} 
+                onChange={(e)=> setSearchText(e.target.value)}/>
+              <button className={styles.searchButton} type="submit">
+                <FaSearchengin size={10}/>
+             </button>
+            </div>
+        </form>
+    );
+}
